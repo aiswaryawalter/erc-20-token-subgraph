b87a37792ff094893ce5406aa88bf45fd0e6b5f3
aiswaryawalter <aiswaryawalter@gmail.com>
2022-06-03 21:05:44 +0530

    initial commit

---
+++ src/mapping.ts
@@ -0,0 +1,60 @@
+import { BigInt } from "@graphprotocol/graph-ts"
+import { PushToken, Transfer, Approval } from "../generated/PushToken/PushToken"
+import { User, UserCounter, TransferCounter } from "../generated/schema"
+
+export function handleTransfer(event: Transfer): void {
+  let contract = PushToken.bind(event.address)
+  // let decimals = BigInt.fromString(contract.decimals.toString())
+  let day = (event.block.timestamp.div(BigInt.fromI32(60 * 60 * 24)))
+
+  let userFrom = User.load(event.params.from.toHex())
+  if (userFrom == null) {
+    userFrom = newUser(event.params.from.toHex(), event.params.from.toHex());
+  }
+  userFrom.balance = (userFrom.balance).minus(event.params.tokens)
+  userFrom.transactionCount = userFrom.transactionCount + 1
+  userFrom.save()
+
+  let userTo = User.load(event.params.to.toHex())
+  if (userTo == null) {
+    userTo = newUser(event.params.to.toHex(), event.params.to.toHex());
+
+    // UserCounter
+    let userCounter = UserCounter.load('singleton')
+    if (userCounter == null) {
+      userCounter = new UserCounter('singleton')
+      userCounter.count = 1
+    } else {
+      userCounter.count = userCounter.count + 1
+    }
+    userCounter.save()
+    userCounter.id = day.toString()
+    userCounter.save()
+  }
+  userTo.balance = (userTo.balance).plus(event.params.tokens)
+  userTo.transactionCount = userTo.transactionCount + 1
+  userTo.save()
+
+  // Transfer counter total and historical
+  let transferCounter = TransferCounter.load('singleton')
+  if (transferCounter == null) {
+    transferCounter = new TransferCounter('singleton')
+    transferCounter.count = 0
+    transferCounter.totalTransferred = BigInt.fromI32(0)
+  }
+  transferCounter.count = transferCounter.count + 1
+  transferCounter.totalTransferred = transferCounter.totalTransferred.plus(event.params.tokens)
+  transferCounter.save()
+  transferCounter.id = day.toString()
+  transferCounter.save()
+}
+
+function newUser(id: string, address: string): User {
+  let user = new User(id);
+  user.address = address
+  user.balance = BigInt.fromI32(0)
+  user.transactionCount = 0
+  return user
+}
+
+export function handleApproval(event: Approval): void {}
